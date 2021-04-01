f2cc3526926bbe6d93ed12b2e8037d52e5a1a3c0
Rohin Thampilali Narayan <rnarayan@login05.cm.cluster>
2021-04-01 14:18:40 -0500

    changing batch submission script:  format changed to sbatch --array=XX-YY submitToBatch.sh /users/rnarayan/Public/geant4-py/Geant4-GUI-V2.3/run.sh

--- Geant4-GUI-V2.3/submitToBatch.sh
+++ Geant4-GUI-V2.3/submitToBatch.sh
@@ -7,11 +7,7 @@
 
 module load singularity 
 module load spack
-#singularity   shell -B /cvmfs:/cvmfs -B /hpc:/hpc /users/rnarayan/Public/geant4-10.6_latest.sif -b
-#source /hpc/spack/opt/spack/linux-centos7-x86_64/gcc-6.3.0/geant4-10.04.p02-us2el5gppxoev2mzh62ytjwhylzw6cjj/share/Geant4-10.4.2/geant4make/geant4make.sh
-#source /app/geant4/geant4-10.6.1-install/bin/geant4.sh
+#singularity exec -B /cvmfs:/cvmfs -B /hpc:/hpc /users/rnarayan/Public/geant4-10.6_latest.sif /users/rnarayan/Public/geant4-py/Geant4-GUI-V2.3/run.sh $SLURM_ARRAY_TASK_ID
+singularity exec -B /cvmfs:/cvmfs -B /hpc:/hpc /users/rnarayan/Public/geant4-10.6_latest.sif $1 $SLURM_ARRAY_TASK_ID
 
-#python Geant4-BatchV.py --Batch --Setupfile BatchModeSetupFiles/defaultsetup.yml --Seed $SLURM_TASK_ID
-
-singularity exec -B /cvmfs:/cvmfs -B /hpc:/hpc /users/rnarayan/Public/geant4-10.6_latest.sif /users/rnarayan/Public/geant4-py/Geant4-GUI-V2.3/run.sh $SLURM_ARRAY_TASK_ID
 
